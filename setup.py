fc87c51023ef9be43f0090f333c0da479233120c
Thomas Lovén <thomasloven@gmail.com>
2023-07-15 23:12:13 +0000

    Prepare for pypi

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages, setup
+
+MIN_PY_VERSION = "3.10"
+PACKAGES = find_packages()
+VERSION = "0.1"
+
+setup(
+    name="pyplejd",
+    packages=PACKAGES,
+    version=VERSION,
+    description="A python library for communicating with Plejd devices via bluetooth",
+    author="Thomas Lovén",
+    author_email="thomasloven@gmail.com",
+    license="MIT",
+    url="https://github.com/thomasloven/pyplejd",
+    download_url=f"https://github.com/thomasloven/pyplejd/archive/v{VERSION}.tar.gz",
+    install_requires=["aiohttp"],
+    keywords=["plejd", "bluetooth", "homeassistant"],
+    python_requires=f">={MIN_PY_VERSION}",
+)
