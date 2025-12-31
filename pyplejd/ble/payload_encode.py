ee38aacb6069d3709e2cbe735cf0cffae05256aa
Thomas Lovén <thomasloven@gmail.com>
2025-12-31 01:03:18 +0100

    Remove dead code

--- pyplejd/ble/payload_encode.py
+++ pyplejd/ble/payload_encode.py
@@ -12,70 +12,8 @@ from .crypto import encrypt_decrypt
 from .debug import send_log
 
 
-def encode(mesh: PlejdMesh, payloads: list[str]):
-    return [
-        encrypt_decrypt(
-            mesh._crypto_key,
-            mesh._gateway_node,
-            binascii.a2b_hex(payload.replace(" ", "")),
-        )
-        for payload in payloads
-    ]
-
-
 def hex_payload(payload):
     return "".join([f"{b:02x}" for b in binascii.a2b_hex(payload.replace(" ", ""))])
-
-
-def set_state(mesh: PlejdMesh, address, **state):
-    payloads = []
-
-    if (st := state.get("state", None)) is not None:
-        if st:
-            if (dim := state.get("dim", None)) is not None:
-                # Dim command
-                # AA 0110 0098 01 DDDD
-                # Dim level is passed twice to transform it from 1 byte to 2
-                payloads.append(f"{address:02x} 0110 0098 01 {dim:02x}{dim:02x}")
-                send_log(f"DIM command {hex_payload(payloads[-1])}", address)
-            else:
-                # State command on
-                # AA 0110 0097 01
-                payloads.append(f"{address:02x} 0110 0097 01")
-                send_log(f"ON command {hex_payload(payloads[-1])}", address)
-        else:
-            # State command off
-            # AA 0110 0097 00
-            payloads.append(f"{address:02x} 0110 0097 00")
-            send_log(f"OFF command {hex_payload(payloads[-1])}", address)
-
-    if (ct := state.get("colortemp", None)) is not None:
-        # Color temperature command
-        # AA 0110 0420 030111 TTTT
-        payloads.append(f"{address:02x} 0110 0420 030111 {ct:04x}")
-        send_log(f"COLORTEMP command {hex_payload(payloads[-1])}", address)
-
-    if (cover := state.get("cover", None)) is not None:
-        if cover < 0:
-            # Cover command stop
-            # AA 0110 0420 030807 00
-            payloads.append(f"{address:02x} 0110 0420 030807 00")
-            send_log(f"COVER STOP command {hex_payload(payloads[-1])}", address)
-        else:
-            # Cover command position
-            # AA 0110 0420 030827 01 PPPP
-            payloads.append(f"{address:02x} 0110 0420 030827 01 {cover:04x}")
-            send_log(f"COVER POSITION command {hex_payload(payloads[-1])}", address)
-
-    return encode(mesh, payloads)
-
-
-# def trigger_scene(mesh: PlejdMesh, index):
-#     # Scene trigger command
-#     # 02 0110 0021 II
-#     payload = f"02 0110 0021 {index:02x}"
-#     send_log(f"SCENE command {hex_payload(payload)}", "SCN")
-#     return encode(mesh, [payload])
 
 
 def set_time(mesh: PlejdMesh):
@@ -103,12 +41,3 @@ def request_time(mesh: PlejdMesh, address):
     send_log(f"TIME REQUEST {hex_payload(payload)}", "TME")
     return payload
     return encode(mesh, [payload])
-
-
-# def request_button(mesh: PlejdMesh):
-#     # Request button identification
-#     # 00 0110 0015
-
-#     payload = f"00 0110 0015"
-#     send_log(f"IDENTIFY BUTTON REQUEST {hex_payload(payload)}")
-#     return encode(mesh, [payload])
